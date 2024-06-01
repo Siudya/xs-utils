acbdb831de80606c53c4f525793e65ee0b1a08ed
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2024-06-01 11:09:43 +0800

    Optimize codes of clock gate for simulation

--- src/main/resources/STD_CLKGT_func.v
+++ src/main/resources/STD_CLKGT_func.v
@@ -6,28 +6,9 @@ module STD_CLKGT_func (
   input  wire dft_l3dataram_clk,
   input  wire dft_l3dataramclk_bypass
 );
-
-  wire clk_en;
-  reg  clk_en_reg;
-
-  assign clk_en = E | TE;
-  assign Q = CK & clk_en_reg;
-
-`ifdef VCS
-  always @(CK or clk_en)
-  begin
-    if(CK == 1'b0) clk_en_reg <= clk_en;
+  reg EN;
+  always_latch begin
+    if(!CK) EN = TE | E;
   end
-`elsif VERILATOR_5016
-  always @(CK or clk_en)
-  begin
-    if(CK == 1'b0) clk_en_reg <= clk_en;
-  end
-`else
-  always @(posedge CK) 
-  begin
-    clk_en_reg = clk_en;
-  end
-`endif
-
-endmodule // Copy from Xihu
+  assign Q = CK & EN;
+endmodule
