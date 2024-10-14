4953991a5196d923b2679f237197577f7413721c
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2024-10-14 18:16:31 +0800

    feat: Add more ctrl signal for PLL

--- src/main/scala/xs/utils/ClockManagerWrapper.scala
+++ src/main/scala/xs/utils/ClockManagerWrapper.scala
@@ -5,30 +5,30 @@ import chisel3.util.HasBlackBoxInline
 
 class ClockManagerWrapper extends BlackBox with HasBlackBoxInline {
   val io = IO(new Bundle {
-    val cfg = Input(Vec(8, UInt(64.W)))
+    val cfg = Input(Vec(8, UInt(32.W)))
     val in_clock = Input(Clock())
     val cpu_clock = Output(Clock())
-    val timer_clock = Output(Clock())
+    val lock = Output(Bool())
   })
   private val modName = s"${GlobalData.prefix}ClockManagerWrapper"
   override val desiredName = modName
   setInline(s"$modName.sv",
     s"""
        |module $modName (
-       |  input  wire [63:0] cfg_0,
-       |  input  wire [63:0] cfg_1,
-       |  input  wire [63:0] cfg_2,
-       |  input  wire [63:0] cfg_3,
-       |  input  wire [63:0] cfg_4,
-       |  input  wire [63:0] cfg_5,
-       |  input  wire [63:0] cfg_6
-       |  input  wire [63:0] cfg_7,
+       |  input  wire [31:0] cfg_0,
+       |  input  wire [31:0] cfg_1,
+       |  input  wire [31:0] cfg_2,
+       |  input  wire [31:0] cfg_3,
+       |  input  wire [31:0] cfg_4,
+       |  input  wire [31:0] cfg_5,
+       |  input  wire [31:0] cfg_6
+       |  input  wire [31:0] cfg_7,
        |  input  wire in_clock,
        |  output wire cpu_clock,
-       |  output wire timer_clock
+       |  output wire lock
        |);
-       |  assign cpu_clock = in_clock,
-       |  assign timer_clock = in_clock
+       |  assign cpu_clock = in_clock;
+       |  assign lock = 1'b1;
        |endmodule
        |""".stripMargin)
 }
